ea284cc29449ee162186c0d3eb913b61b66e591c
damian.sancristobal <damian.sancristobal@fing.edu.uy>
2019-08-20 18:32:56 -0300

    45 a 43

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,5 @@
 class StatusController < ApplicationController
     def index
-        render(json: { 'status' => 'ok' , "data" => 45})
+        render(json: { 'status' => 'ok' , "data" => 42})
     end
 end
