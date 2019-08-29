d4713082e68e02f2e9f1fd8ea49c3ce9429d5124
Lucas Barenchi Gileno <lucas.barenchi@fing.edu.uy>
2019-08-28 21:50:54 -0300

    check if fails

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'Status request' do
-    #describe 'GET /status' do
-    #    it 'returns a status message' do
-    #        get('/status')
-    #        json = JSON.parse(response.body)
-    #        expect(json['status']).to eql('ok')
-    #    end
-    #end
+    describe 'GET /status' do
+        it 'returns a status message' do
+            get('/status')
+            json = JSON.parse(response.body)
+            expect(json['status']).to eql('ok')
+        end
+    end
 end
