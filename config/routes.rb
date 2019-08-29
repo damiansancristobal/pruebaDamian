a0ed7d1d13c57d3ae33dc17df116f5b6b16bcdf0
Lucas Barenchi Gileno <lucas.barenchi@fing.edu.uy>
2019-08-29 11:04:02 -0300

    check if pr cov works

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # devise_for :users
   get('status' => 'status#index')
   get '/testing/:id', to: 'status#test'
+  get '/testing/work', to: 'status#testing'
   resources :sessions, only: [:create,:destroy]
 end
