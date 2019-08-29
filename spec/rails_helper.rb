a89bd9a3c8f3cb03a74b572bc56c2d98ef57353a
Lucas Barenchi Gileno <lucas.barenchi@fing.edu.uy>
2019-08-28 21:10:36 -0300

    asd

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 ENV['RAILS_ENV'] ||= 'test'
+ENV['CODECOV_TOKEN'] ||= '49f58adb-ee76-49f5-843d-87865e4059f6'
 require File.expand_path('../../config/environment', __FILE__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
